70a89c174b54520b0be638f0b6638ebea43cba17
Chris <chris@chris-industries.de>
2025-01-13 12:55:19 +0100

    changed script in DBS Praktikum

--- DBS/Praktikum/03/script.sql
+++ DBS/Praktikum/03/script.sql
@@ -1,18 +1,34 @@
-Create Table hersteller (
-    hnr VARCHAR(4),
-    name VARCHAR(30),
-    stadt VARCHAR(30),
-    PRIMARY KEY (hnr)
+Create Table regal (
+    regalnr Numeric primary key,
+    lage varchar(5),
+    platzgesamt Integer not null,
+    platzbelegt interger Default 0
 );
 
-Create Table produkt (
-    pnr VARCHAR(4),
-    name VARCHAR(30),
-    hnr VARCHAR(4),
-    preis NUMERIC(8,2),
-    PRIMARY KEY (pnr)
-):
+create table ware(
+    invnr interger primary key,
+    regalnr interger not null,
+    warenname varchar(50),
+    typ varchar(30),
+    anzahl interger,
+    platzproeoinheit interger,
+    preisproeinheit numeric(8,2),
+    datumverfuegbar date Default current_date,
+    foreign key (regalnr) references regal(regalnr)
+);
 
-create table lieferung(
-    
-)
+create table warehistory (
+    invnr interger,
+    regalnr interger,
+    warenname varchar(50),
+    typ varchar(30),
+    anzahl interger,
+    platzproeoinheit interger,
+    preisproeinheit numeric(8,2),
+    datumverfuegbar date,
+    datumverkauft date Default current_date,
+    foreign key (regalnr) references regal(regalnr)
+);
+
+Create or replace function tigger_ware_modification()
+returns trigger as $$
